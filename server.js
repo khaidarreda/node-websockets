7ca516a917693ac45d948ab63d6555952bb1e70c
mohamed reda khaidar <44505139+khaidarreda@users.noreply.github.com>
2021-10-30 19:27:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express()
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
 
 const wss = new Server({ server });
 
@@ -16,12 +19,12 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-server.post('/', (req, res) => {
+app.post('/', (req, res) => {
   res.sendFile(INDEX, { root: __dirname })
 });
 
 
-server.post('/misba7', (req, res) => {
+app.post('/misba7', (req, res) => {
   let text = req.body.text;
   // implement your bot here ...
   res.send('Got that!');
