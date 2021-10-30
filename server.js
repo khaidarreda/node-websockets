ba5ad7738b551e6a675407cb632cd7fb228cb41d
mohamed reda khaidar <44505139+khaidarreda@users.noreply.github.com>
2021-10-30 19:20:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,26 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+server.post('/', (req, res) => {
+  let text = req.body.text;
+  // implement your bot here ...
+  res.sendFile(INDEX, { root: __dirname })
+});
+
 
 const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+});
+
+server.post('/misba7', (req, res) => {
+  let text = req.body.text;
+  // implement your bot here ...
+  res.send(text);
 });
 
 setInterval(() => {
